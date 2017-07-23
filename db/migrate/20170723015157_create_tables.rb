30e2b1b69ec68c74dacb5d924cc4c8ed8f606442
marclevetin <marc@derbyhat.org>
2017-07-22 23:02:28 -0400

    some tables added.  Need to complete migration.

---
+++ db/migrate/20170723015157_create_tables.rb
@@ -0,0 +1,42 @@
+class CreateTables < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :event_name
+      t.date :start_date
+      t.date :end_date
+      t.string :location
+      t.string :event_director
+      t.string :contest_coordinator
+      t.string :head_judge
+      t.text :event_disclaimer
+
+      t.timestamps, null: false
+    end
+
+    create_table :contests do |t|
+      t.string :title
+      t.datetime :time
+      t.belongs_to :event
+
+      t.timestamps, null: false
+    end
+
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :wsdc_id
+
+      t.belongs_to :event
+
+      t.timestamps, null: false
+    end
+
+    create_table :judges do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.belongs_to :event
+    end
+  end
+end
