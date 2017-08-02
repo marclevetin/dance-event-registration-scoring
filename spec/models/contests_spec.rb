bac3e841600594103e6b4b4dd2e4f1e414a452c6
marclevetin <marc@derbyhat.org>
2017-08-02 13:07:02 -0400

    added tests for contests, judges

--- spec/models/contests_spec.rb
+++ spec/models/contests_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 
 RSpec.describe Contest, type: :model do
   let(:contest) { Contest.new(title: "title") }
+  let(:contest_without_required_fields) {Contest.new(title: nil)}
 
   describe "contest model validations" do
     it "contest is valid with all required fields" do
-      expect(contest.save).to eq(true)
+      expect(contest.title).to eq("title")
     end
 
     it "contest is not valid if title is missing" do
-      expect(contest.save).to eq(false)
+      expect(contest_without_required_fields.save).to eq(false)
     end
 
   end
