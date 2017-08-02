ec37c5723f521ed13e11a68c3f9f0c69f951766f
marclevetin <marc@derbyhat.org>
2017-08-02 00:07:01 -0400

    updated validations on person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ApplicationRecord
-  validates first_name, presence: true
-  validates last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :wsdc_id, numericality: true
 
   belongs_to :event
